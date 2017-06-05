ee221e53cc1c3195a4e27a070306a406397c0d1b
dheckerzone <dhericklubigan@gmail.com>
2017-06-05 08:07:47 +0800

    Initial

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','http://www.cryingoverspiltmilk.co.nz/wp-content/uploads/2015/03/pixabay/b/recipe_1426460443.png'),
+    new Recipe('Another Test Recipe','This is simply a test','http://www.cryingoverspiltmilk.co.nz/wp-content/uploads/2015/03/pixabay/b/recipe_1426460443.png')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+}
